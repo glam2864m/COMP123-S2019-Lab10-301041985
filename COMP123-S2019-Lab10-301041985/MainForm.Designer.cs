635b8578399e97ff750c14b76fa078cb0908f289
glam2864m@yahoo.com.tw <glam2864m@yahoo.com.tw>
2019-07-19 15:49:59 -0400

    Lab10

---
+++ COMP123-S2019-Lab10-301041985/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace COMP123_S2019_Lab10_301041985
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(160, 82);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(445, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Main Form!";
+            // 
+            // NextButton
+            // 
+            this.NextButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.NextButton.Location = new System.Drawing.Point(255, 405);
+            this.NextButton.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.NextButton.Name = "NextButton";
+            this.NextButton.Size = new System.Drawing.Size(219, 73);
+            this.NextButton.TabIndex = 1;
+            this.NextButton.Text = "Next";
+            this.NextButton.UseVisualStyleBackColor = false;
+            this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(23F, 46F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(778, 544);
+            this.Controls.Add(this.NextButton);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NextButton;
+    }
+}
+
